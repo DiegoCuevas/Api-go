6a8c79b9570130d025642b700f2626a4fc1c8814
Diego Cuevas <diegocuevas579@gmail.com>
2022-10-12 09:12:18 -0500

    ADD: main file

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"encoding/json" // modulo la codificacion y la decodificacion de Json
+	"log"            // modulo para debuggear
+	"net/http"       // modulo escribir las peticiones http
+	"github.com/gorilla/mux" // modulo para las rutas
+)
+
+
+func main()  {
+	
+}
